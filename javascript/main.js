48f99a3b567b6e557e3b2b05240f91d976028f33
Marius Grondahl <marius@grondahldesign.no>
2019-07-08 18:34:14 +0200

    collission working

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,15 @@
 // Lets start this shit
 var gameBoard = new Game (400, 600);
 var surfer = new Surfer (200, 500);
-var shark = new Shark (50, 0);
 
+var shark = new Shark (50, 0, "sharks");
 
 // All the interaction
 let gameContainer = document.getElementById("game-container");
 let surferContainer = document.getElementById("surfer");
-let sharkContainer = document.getElementById("shark");
+let sharkContainer = document.querySelector(".shark");
+
+
 let gameTitle =  document.getElementById("game-title");
 let gameMessage =  document.getElementById("game-message");
 let message = document.getElementById("message");
@@ -19,9 +21,6 @@ gameContainer.style.height = gameBoard.gameheight + "px";
 
 surferContainer.style.width = surfer.width + "px";
 surferContainer.style.height = surfer.heigth + "px";
-
-sharkContainer.style.height = shark.heigth + "px";
-sharkContainer.style.width = shark.width + "px";
 
 // Her`s our Australian Surferboy
 function getSurfer() {
@@ -42,6 +41,15 @@ function getSharks() {
 }
 
 getSharks();
+
+
+/////////////// Lets work with sharkclass
+
+// Here is our random number
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 
 
 
--- javascript/surf.js
+++ javascript/surf.js
@@ -45,17 +45,30 @@ class Surfer {
   }
 } // Her we end surfer
 
-
 // Lets create a shark
 class Shark {
-  constructor (x, y) {
+  constructor (x, y, classname) {
       this.x = x;
       this.y = y;
       this.width = 25;
       this.height = 40;
       this.interval = 0;
-      this.movingState = 0;
       this.collisions = 0;
+      this.movingState = 0;
+      this.classname = classname;
+      
+    }
+
+    create() {
+      var div = document.createElement("div");
+      div.style.width = this.width + "px";
+      div.style.height = this.heigth + "px";
+      div.style.marginLeft = 25 + "px";
+      div.className = this.classname;
+
+      setInterval(() => {
+        document.getElementById("game-container").appendChild(div);
+      }, 2);
     }
 
     move() {
@@ -63,16 +76,23 @@ class Shark {
         this.y = this.y - 2;
         this.movingState = 1;
         this.collision();
+        if (this.y < -560 ){
+          this.reset();
+          this.x = this.x + 100;
+          this.move();
+        } 
       }, intervalSpeed);
     }
 
     reset() {
       clearInterval(this.interval);
+      clearInterval(this.collisions);
       this.interval = 0;
-      this.y = 0;
+      this.collisions = 0;
       this.movingState = 0;
+      this.y = 0;
     }
-
+    
     collision() {
       let sharkX1 = this.x;
       let sharkX2 = this.x + this.width;
@@ -85,19 +105,30 @@ class Shark {
       let surfY2 = surfer.y + surfer.height;
 
       this.collisions = setInterval(() => {
-  
         if (sharkX1 < surfX2 && sharkX2 > surfX1 && 
           sharkY1 < surfY2  && sharkY2 > surfY1) {
           // This will happen to our surfer on collision
           gameBoard.message();
           surfer.reset();
-          // This will happen to Sharks on collison
           this.reset(); 
-        
+          // Reset the mathematics on colission  
+          sharkX1 = this.x;
+          sharkX2 = this.x + this.width;
+          sharkY1 = -this.y;
+          sharkY2 = -this.y + this.height;
+    
+          surfX1 = surfer.x;
+          surfX2 = surfer.x + surfer.width;
+          surfY1 = surfer.y;
+          surfY2 = surfer.y + surfer.height;
+          
         }
         
       }, intervalSpeed); 
-    }
+    } 
 
 
 }
+
+
+
