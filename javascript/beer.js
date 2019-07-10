7933325ee316a45c3a6cd08577ea33e3a98451da
Marius Grondahl <marius@grondahldesign.no>
2019-07-10 21:42:23 +0200

    Removed baaaad wav sound

--- javascript/beer.js
+++ javascript/beer.js
@@ -15,16 +15,16 @@ class Beer {
       this.domRef = div;
     }
 
-    // random number for sharks
+    // random number for Beer
     getRandom(min, max) {
         return (Math.floor(Math.random() * (max - min) + min))  ;
     }
 
-    // Moving and placing the sharks
+    // Moving and placing the Beer
     move() {
-        //adding vertical speed to sharks
-        this.y += 1;
-        // Check if sharks is out of bounds bottom and left/right
+        //adding vertical speed to Beer
+        this.y += 0.5;
+        // Check if beers is out of bounds bottom and left/right
 
         if (this.y > gameBoard.gameheight - 70) {
           this.reset();     
@@ -35,18 +35,18 @@ class Beer {
         // Manipulates the DOM based on the x and y
         this.domRef.style.top =  this.y + "px";
         this.domRef.style.left = this.x + "px";
-        // check for collision when moving
+        // Check if surfboard hits beer
         this.points();
     }
 
-    // Resets the sharks
+    // Resets the Beer
     reset() {
       this.y = 0;   
       this.speed += 1;  
       // These are all the alternate startingpositions so tha we place the sharks according to the surfboard
       let startingPosistions = [0, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375]
       this.x = startingPosistions[this.getRandom(0,16)] ;
-      
+      this.points();
     }
 
     sound() {
@@ -54,7 +54,7 @@ class Beer {
       sound.play();
     }
 
-    // Detects Collision with other sharks
+    // Detects Points for our Beers
     points() {
       let surfX1 = surfer.x;
       let surfX2 = surfer.x + surfer.width;
@@ -63,14 +63,9 @@ class Beer {
 
         if (this.x < surfX2 && this.x + this.width > surfX1 && 
           this.y < surfY2  && this.y + this.height > surfY1) {
-          // This will happen to our shark on collision
-          gameBoard.score++;
-          this.sound();
-
+          // This will when we get points
+           this.reset(); 
+           
         }  
     } 
 }
-
-// setTimeout(() => {
-//   this.y = 0;      
-// }, this.getRandom(1000, 2000));
